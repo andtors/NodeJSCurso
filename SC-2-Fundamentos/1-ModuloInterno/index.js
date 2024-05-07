9b789facc79ab09512d02c0d646b0e0b5071ce1b
André Torres <andre.tors448@gmail.com>
2024-05-07 17:58:51 -0300

    SC-2 Concluido

---
+++ SC-2-Fundamentos/1-ModuloInterno/index.js
@@ -0,0 +1,10 @@
+const meuModulo = require('./meuModulo')
+const soma = meuModulo.soma
+const subtracao = meuModulo.subtracao
+
+soma(2, 3)
+soma(5, 10)
+
+subtracao(20, 10)
+subtracao(50, 25)
+
---
+++ SC-2-Fundamentos/2-ExportImport/index.mjs
@@ -0,0 +1,6 @@
+import soma from './meuModulo.mjs'
+
+soma(2, 3)
+soma(5, 10)
+
+
